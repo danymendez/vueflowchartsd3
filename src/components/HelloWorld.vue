7d285059d3702ea9f9fdfc76a93bee7391315e57
Daniel Mendez <Daniel.Mendez@amsoftware.com>
2022-01-05 01:15:40 -0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body" draggable="true">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+            <div id="zoom" class="col-sm-8 drawing-area" @drop="drop($event)" @dragover.prevent @dragenter.prevent>
+
+            </div>
+            <div class="col-sm-1">
+                <!-- <button type="button" v-on:click="addRect">Add me</button> -->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import * as d3 from 'd3'
+
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                svg: Object,
+                g: Object,
+                data: Object
+            };
+        },
+        props: {
+            msg: String
+        },
+        methods: {
+            init() {
+
+            },
+            drop(event) {
+
+                var svgdoc = d3.select('svg');
+
+                var svgBoundClientRect = svgdoc.node().getBoundingClientRect();
+                console.log(svgBoundClientRect);
+                console.log('x: ' + event.pageX);
+                console.log('y: ' + event.pageY);
+
+                this.data.push({ x: (event.pageX - svgBoundClientRect.x), y: (event.pageY - svgBoundClientRect.y) });
+                this.addRect();
+            },
+            addRect() {
+                let _this = this;
+                d3.selectAll("g").remove();
+                _this.g = _this.svg.append("g").attr("cursor", "grab");
+                _this.g.selectAll("rect")
+                    .data(_this.data)
+                    .join("rect")
+                    .attr("x", ({ x }) => x)
+                    .attr("y", ({ y }) => y)
+                    .attr('width', 40)
+                    .attr('height', 40)
+                    .attr('stroke', 'black')
+                    .attr('fill', '#69a3b2')
+                    .call(d3.drag()
+                        .on("start", function () {
+                            d3.select(this).raise();
+                            _this.g.attr("cursor", "grabbing");
+                        }).on("drag", function (event, d) {
+
+                            d3.select(this).attr("x", d.x = event.x).attr("y", d.y = event.y);
+                        })
+                        .on("end", function () {
+                            _this.g.attr("cursor", "grab");
+                        }));
+                _this.svg.call(d3.zoom()
+                    .extent([[0, 0], [document.getElementById("zoom").offsetWidth, document.getElementById("zoom").offsetHeight]])
+                    .scaleExtent([1, 8])
+                    .on("zoom", function ({ transform }) {
+                        _this.g.attr("transform", transform);
+                    }));
+            }
+        },
+        mounted() {
+            let _this = this;
+            _this.data = [];
+            _this.svg = d3
+                .select("#zoom")
+                .append("svg")
+                .attr("viewBox", [0, 0, document.getElementById("zoom").offsetWidth, document.getElementById("zoom").offsetHeight]);
+
+        }
+    }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
