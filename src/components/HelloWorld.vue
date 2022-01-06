d6d25911a2d813146a20a3d1fac04bd4a874b511
Daniel Mendez <Daniel.Mendez@amsoftware.com>
2022-01-05 21:20:58 -0600

    some improvements

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,8 @@
 
             </div>
             <div class="col-sm-1">
-                <!-- <button type="button" v-on:click="addRect">Add me</button> -->
+                 <button type="button" v-on:click="addLine">Add Line</button>
+                  <button type="button" v-on:click="drawDiagram">Draw me</button>
             </div>
         </div>
     </div>
@@ -37,16 +38,68 @@
             init() {
 
             },
-            drop(event) {
+             drawDiagram(){
+                 var x1 = 40; var w = 100; var h=40; var y1 = 40;
+                 var y3 = y1-h;
+                  this.data.push({ x: 40, y:40 , w: w, h:h , id:1, parent:0, lvl:0});
+                      this.data.push({ x: x1, y:y3 , w: w, h:h, id:2, parent:1,lvl:0 });
+                         this.data.push({ x: 70*5, y:70 , w: w, h:h, id:3, parent:1,lvl:0 });
+                  this.addRect();
+            },
+            addLine(){
+                let _this = this
 
-                var svgdoc = d3.select('svg');
 
-                var svgBoundClientRect = svgdoc.node().getBoundingClientRect();
-                console.log(svgBoundClientRect);
-                console.log('x: ' + event.pageX);
-                console.log('y: ' + event.pageY);
 
-                this.data.push({ x: (event.pageX - svgBoundClientRect.x), y: (event.pageY - svgBoundClientRect.y), w: 100, h:40 });
+const found = _this.data.find(element => element.parent == 0);
+
+console.log(found);
+// const link = Array.from({length: _this.data.length}, (_, i) => {
+//     console.log(_this.data.find(element => element.parent === _this.data[i].parent).x);
+//   return {
+//    source: [_this.data.find(element => element.parent === _this.data[1].parent).x,_this.data.find(element => element.parent === _this.data[1].parent).y],
+//     target: [_this.data[1].x,_this.data[1].y]
+//   };});
+for(var i =0;i<_this.data.length;i++){
+// const link = d3.linkHorizontal()({
+//                                   source: [_this.data.find(element => element.parent === _this.data[i].parent).x,_this.data.find(element => element.parent === _this.data[i].parent).y],
+//                                    target: [_this.data[i].x,_this.data[i].y]
+//                                 });
+if(_this.data[i].parent!=0){
+                _this.g
+                        .append('path')
+                        .attr('d', d3.linkHorizontal()({
+                                  source: [_this.data.find(element => element.id === _this.data[i].parent).x,_this.data.find(element => element.id === _this.data[i].parent).y],
+                                   target: [_this.data[i].x,_this.data[i].y]
+                                }))
+                        .attr('stroke', 'black')
+                        .attr('fill', 'none');
+
+
+
+                        }
+
+}
+// console.log(link);
+
+
+            },
+            drop() {
+
+                // var svgdoc = d3.select('svg');
+
+                // var svgBoundClientRect = svgdoc.node().getBoundingClientRect();
+                // console.log(svgBoundClientRect);
+                // console.log('x: ' + event.pageX);
+                // console.log('y: ' + event.pageY);
+
+                // this.data.push({ x: (event.pageX - svgBoundClientRect.x), y: (event.pageY - svgBoundClientRect.y), w: 100, h:40 });
+                // if(this.data.length===0){
+                //     this.data.push({ x: 40, y: 40, w: 100, h:40 });
+                // }else{
+                //      this.data.push({ x: this.data.length%4===0?40:(this.data[this.data.length-1].x+(40*5)), y: this.data.length%4===0?(this.data[this.data.length-1].y+(40*3)):this.data[this.data.length-1].y, w: 100, h:40 });
+                // }
+                // console.log(this.data);
                 this.addRect();
             },
             addRect() {
